23f269d53f82be8d371ec1aa4e203f0cd0191045
SHRAVANI KAWARE <Shravanikaware12@gmail.com>
2023-12-11 22:22:13 +0530

    commit

--- src/Result.js
+++ src/Result.js
@@ -1,24 +1,16 @@
-// Result.js
+import React, { useMemo } from 'react'
+import { Typography, Card, CardContent, CardActions, Button } from '@mui/material'
 
-import React, { useMemo } from 'react';
-import { Typography, Card, CardContent, CardActions, Button } from '@mui/material';
-import { useSelector, useDispatch } from 'react-redux';
-import questions  from './data/questions'; 
+import questions from "./data/questions";
 
-export default function Result() {
-    const answers = useSelector((state) => state.answers);
-    const dispatch = useDispatch();
+export default function Result(props) {
+    const { answers, restartQuiz } = props;
 
     const correctAnswers = useMemo(() => {
         return questions.filter((q, i) => {
             return q.correctAnswer === parseInt(answers[i]);
         }).length;
-    }, [answers]);
-
-    const restartQuiz = () => {
-        dispatch({ type: 'RESET_QUIZ' });
-        // Perform any other necessary actions for resetting the quiz state
-    };
+    }, [answers])
 
     return (
         <Card variant='outlined' sx={{ pt: 3, pb: 3 }}>
@@ -31,7 +23,9 @@ export default function Result() {
                 </Typography>
             </CardContent>
             <CardActions sx={{ display: "flex", justifyContent: "center" }}>
-               
+                <Button onClick={restartQuiz} variant="outlined">
+                    Retry
+                </Button>
             </CardActions>
         </Card>
     )
